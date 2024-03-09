02352b304e91ba6405d8ddff05e99ef404599d80
anair123 <aashishnair8925@gmail.com>
2024-03-09 00:27:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,16 +13,22 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 import chainlit as cl
 
-def create_llm():
+def create_client():
+    bedrock = boto3.client(service_name='bedrock-runtime')
+    return bedrock
 
-    llm = Bedrock(model_id='meta.llama2-13b-chat-v1', client=bedrock,
+def create_llm(bedrock):
+    llm = Bedrock(model_id='meta.llama2-13b-chat-v1', 
+                  client=bedrock,
+                  streaming=True,
                   model_kwargs={'temperature':1})
     return llm
 
 def create_prompt():
 
     prompt_template = """
-    Use the provided information to answer the users questions. If you do not have the context required to answer the question, respond with "I don't know."
+    Use the provided information to answer the users questions. 
+    If you do not have the context required to answer the question, respond with "I don't know."
 
     {context}
 
@@ -35,13 +41,15 @@ def create_prompt():
 
     return prompt 
 
-def generate_response(llm, vector_store, prompt, query):
+def create_qa_chain(llm, vector_store, prompt):
 
     qa_chain = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', 
                                            retriever=vector_store.as_retriever(search_type='similarity', search_kwargs={"k":3}),
                                            return_source_documents=True,
                                            chain_type_kwargs={'prompt':prompt})
-    
+    return qa_chain
+
+def generate_response(qa_chain, query):
     response = qa_chain({"query":query})
 
     return response['result']
@@ -49,14 +57,51 @@ def generate_response(llm, vector_store, prompt, query):
 
 def main():
 
+    bedrock = boto3.client(service_name='bedrock-runtime')
+    llm = create_llm(bedrock=bedrock)
+    prompt = create_prompt()
+    bedrock_embeddings=BedrockEmbeddings(model_id='amazon.titan-embed-text-v1', client=bedrock)
+
     query = 'What are some techniques that can be uses for interacting with tabular data with LLMs?'
 
-    llm = create_llm()
-    prompt = create_prompt()
-    vector_store = FAISS.load_local('chroma_index')
+    vector_store = FAISS.load_local('faiss_index', bedrock_embeddings, allow_dangerous_deserialization=True)
     response = generate_response(llm=llm, 
                                  vector_store=vector_store, 
                                  query=query,
                                  prompt=prompt)
 
     print(response)
+
+
+### start the app 
+@cl.on_chat_start
+async def start():
+     
+    bedrock = create_client()
+    llm = create_llm(bedrock=bedrock)
+    prompt = create_prompt()
+    bedrock_embeddings=BedrockEmbeddings(model_id='amazon.titan-embed-text-v1', client=bedrock)
+    vector_store = FAISS.load_local('faiss_index', bedrock_embeddings, allow_dangerous_deserialization=True)
+
+
+    qa_chain = create_qa_chain(llm=llm, prompt=prompt, vector_store=vector_store)
+    msg = cl.Message(content="Loading the bot...")
+    await msg.send()
+    msg.content = "Hi, Welcome to the Chatbot! Please ask your question."
+    await msg.update()
+    cl.user_session.set("chain", qa_chain)
+
+
+@cl.on_message
+async def main(message):
+    chain = cl.user_session.get("chain")
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens= ["Final", "Answer"]
+    )
+    cb.answer_reached=True
+
+    res=await chain.acall(message.content, callbacks=[cb])
+    answer = res["result"]
+    sources = res['source_documents']
+
+    await cl.Message(content=answer).send()
