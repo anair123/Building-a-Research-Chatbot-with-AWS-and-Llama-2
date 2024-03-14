96c5d0e400c271db0377606fb099307a55b055c8
anair123 <aashishnair8925@gmail.com>
2024-03-13 23:35:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,14 @@ from langchain.memory import ChatMessageHistory, ConversationBufferMemory
 from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
 import chainlit as cl
+from config import aws_access_key, aws_region_name, aws_secret_key
 
 def create_client():
-    bedrock = boto3.client(service_name='bedrock-runtime')
+    bedrock = boto3.client(service_name='bedrock-runtime',
+                        region_name=aws_region_name,
+                        aws_access_key_id =aws_access_key,
+                        aws_secret_access_key =aws_secret_key
+                        )
     return bedrock
 
 def create_llm(bedrock_client):
@@ -49,15 +54,18 @@ def create_prompt():
 @cl.on_chat_start
 async def create_qa_chain():
 
-    bedrock_client = boto3.client(service_name='bedrock-runtime')
+    # create client 
+    bedrock_client = create_client()
 
+    # load llm
     llm = create_llm(bedrock_client=bedrock_client)
 
+    # load embeddings and vector store
     bedrock_embeddings=BedrockEmbeddings(model_id='amazon.titan-embed-text-v1', client=bedrock_client)
     vector_store = FAISS.load_local('faiss_index', bedrock_embeddings, allow_dangerous_deserialization=True)
     
+    # create memory history
     message_history = ChatMessageHistory()
-
     memory = ConversationBufferMemory(
         memory_key="chat_history",
         output_key="answer",
@@ -65,13 +73,15 @@ async def create_qa_chain():
         return_messages=True,
     )
 
+    # create qa chain
     qa_chain = ConversationalRetrievalChain.from_llm(llm, 
                                            chain_type='stuff', 
                                            retriever=vector_store.as_retriever(search_type='similarity', search_kwargs={"k":3}),
                                            return_source_documents=True,
-                                           #chain_type_kwargs={'prompt':prompt}
                                            memory=memory
                                            )
+    
+    # add custom messages
     msg = cl.Message(content="Loading the bot...")
     await msg.send()
     msg.content = "Hi, Welcome to the QA Chatbot! Please ask your question."
@@ -90,19 +100,18 @@ async def generate_response(query):
         )])
 
     # extract results and source documents
-    #result, source_documents = res['result'], res['source_documents']
     result, source_documents = res['answer'], res['source_documents']
+
     # Extract all values associated with the 'metadata' key
     source_documents = str(source_documents)
     metadata_values = re.findall(r"metadata={'source': '([^']*)', 'page': (\d+)}", source_documents)
 
     # Convert metadata_values into a single string
     pattern = r'PDF Documents|\\'
-
     metadata_string = "\n".join([f"Source: {re.sub(pattern, '', source)}, page: {page}" for source, page in metadata_values])
 
+    # add metadata (i.e., sources) to the results
     result += f'\n\n{metadata_string}'
 
-    print(result)
-
+    # send the generated response to the user
     await cl.Message(content=result).send()
